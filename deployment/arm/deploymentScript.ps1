ab6cfb0c2bb07bfc2b8ca62a488aab67b3fe8ecc
neilpeterson <neilpeterson@outlook.com>
2020-06-23 11:56:51 -0700

    script update

--- deployment/arm/deploymentScript.ps1
+++ deployment/arm/deploymentScript.ps1
@@ -6,12 +6,12 @@ param(
 
 # Install Azure CLI
 bash -c "curl -sL https://aka.ms/InstallAzureCLIDeb | bash"
+bash -c "az login --identity"
 
 # Create storage queue
 az storage queue create --name $storageAccountName --account-name $storageAccountName
 
 # Connect to AKS Cluster
-bash -c "az login --identity"
 bash -c "az aks install-cli"
 bash -c "az aks get-credentials --name $aksCluster --resource-group $aksResourceGroup"
 bash -c "kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/deployment-rbac.yaml"
